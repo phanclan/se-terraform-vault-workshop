c28d8d46883e87d44cc411470849015a203a8061
Peter Phan <pphan@hashicorp.com>
2019-07-01 14:00:20 -0700

    Replaced SSH pub string

--- aws/main.tf
+++ aws/main.tf
@@ -134,7 +134,8 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_key_pair" "tf_ec2_key" {
   key_name = "${var.prefix}tf_ec2_key"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCjOXiqjoBMlfCBvmG6BcUGPv1q+YqNYLHlm6X18Frue+Yf2zG/56pMWtSoPbHKB+Nul0VNpANuOyt3qsEU+HtZz9MMTBiWL6kGH6S0saLMp7EpcZaib/Qxfkl1By6JnOwr6w7eW+XE4TXHRdBKaRWW4J52KdhlPXAeMFeSDL3qnZWaP7tIyKTQzdDXu0rSJIBpcYCVCQ5BkshWNvoVpDH0dH9r4ayLrzgnNzQHyqVFASU3DxqIAqrC3JflAz1aUWiwXhDJeZU3w6eDWvYxOAm+Z2vP5oiX/pqbYMlCUlPrsU5+6828kDQ5uQaZiCnSi2Bj3BDqpJngiVvyicJgvhW9 pephan@Mac-mini.local"
+#  public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
 
 # module "ssh-keypair-aws" {
